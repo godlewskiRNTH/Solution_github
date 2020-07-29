f595e06a854edf33c97b2029b429c418bfeebd92
Keith Godlewski <keith.godlewski@rps30.k12.ar.us>
2020-07-29 15:37:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    //This is a solution to a problem
+    int cases = 1000;
+    int count = 0;
+    for(int x = 1; x < cases; x++)
+    {
+      if(x % 3 == 0 || x % 5 == 0)
+      {
+        count = count + x;
+      }
+    }
+    System.out.println("The sum of all integers under " + cases + " and divisible by 3 or 5 is: " + count);
+  }
+}
